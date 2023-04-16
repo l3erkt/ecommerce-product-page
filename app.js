a470d8734674dce18fc78dbb8f7e0509e1b49477
Bereket Zeggai <bereketzeggai52@gmail.com>
2023-04-16 17:53:05 -0400

    implemented - increment product amount

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const menuBtn = document.getElementById("menubtn");
 const closeBtn = document.getElementById("closebtn");
 const navlinks = document.getElementById("navlinks");
 const darkBkg = document.querySelector(".dark-bkg");
-
 const navToggle = () => {
   menuBtn.addEventListener("click", () => {
     navlinks.style.transform = "unset";
@@ -22,7 +21,6 @@ const previousBtn = document.getElementById("previous");
 const nextBtn = document.getElementById("next");
 const productImg = document.querySelector(".product-image");
 const thumbnail = document.querySelectorAll(".thumbnail");
-
 const carousel = () => {
   // mobile
   let i = 1;
@@ -56,3 +54,43 @@ const carousel = () => {
   });
 };
 carousel();
+
+// input increment
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const cartInput = document.querySelector(".cart-input");
+let n = 0;
+cartInput.addEventListener("input", (e) => {
+  const input = parseInt(e.target.value);
+  if (input < 0) {
+    return (n = 0);
+  } else {
+    n = input;
+    basket(n);
+    return n;
+  }
+});
+const numIncrement = () => {
+  minus.addEventListener("click", () => {
+    if (n === 0) {
+      basket(n);
+      cartInput.value = 0;
+    } else {
+      n--;
+      basket(n);
+      cartInput.value = n;
+    }
+  });
+
+  plus.addEventListener("click", () => {
+    n++;
+    basket(n);
+    cartInput.value = n;
+  });
+};
+numIncrement();
+
+
+// cart basket
+const basket = (quantity) => {
+}
